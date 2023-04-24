24775c7363a0b882f85679b2219861621f71bfc5
Gundaram Raju Rohith <87968757+RohithGundaram@users.noreply.github.com>
2023-04-24 12:15:56 +0530

    Home Page styling done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,27 +4,30 @@ import { BACKEND_URI } from "../config/constants";
 import { ref, deleteObject } from "firebase/storage";
 import { storage } from "../firebase"
 import { useNavigate } from "react-router-dom";
+import "../static/Card.css"
+import "../static/index.css"
+
 export const Cards = (props) => {
   const navigate = useNavigate();
-  const handleClick=(e)=>{
-      e.preventDefault();
-      navigate(`/media/${props.media._id}`)
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate(`/media/${props.media._id}`)
   }
-  const handleDelete=async(id)=>{
+  const handleDelete = async (id) => {
     let newMedia = props.medias.filter((med) => { return med._id !== id })
-    let delMedia=props.medias.filter((med)=>{return med._id === id})
+    let delMedia = props.medias.filter((med) => { return med._id === id })
     // console.log(delMedia.videos)
     let text = delMedia[0].videos
-    let result = parseInt(text.lastIndexOf('o/')+2);
-    let result1 ;
-    if(parseInt(text.lastIndexOf('.mp4')) === -1)
-      result1=parseInt(text.lastIndexOf('.mp3')+4);
+    let result = parseInt(text.lastIndexOf('o/') + 2);
+    let result1;
+    if (parseInt(text.lastIndexOf('.mp4')) === -1)
+      result1 = parseInt(text.lastIndexOf('.mp3') + 4);
     else
-      result1=parseInt(text.lastIndexOf('.mp4')+4);
+      result1 = parseInt(text.lastIndexOf('.mp4') + 4);
 
-    let ans=text.substr(result,result1-result)
+    let ans = text.substr(result, result1 - result)
 
-    let storageRef = ref(storage,ans)
+    let storageRef = ref(storage, ans)
     console.log(storageRef)
     deleteObject(storageRef).then(() => {
       alert("deleted successfully")
@@ -37,19 +40,29 @@ export const Cards = (props) => {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        "auth-token" : localStorage.getItem('token')
+        "auth-token": localStorage.getItem('token')
       },
       // body: JSON.stringify(data),
     });
   }
   return (
-    <div className="col-md-4">
-      <div className="card my-2">
-        <div className="card-body">
-          <h5 className="card-title">{props.media.name}</h5>
-          <div className="flex-row gap-5"><div><h5 className="card-text">-- By {props.media.speaker}</h5><div>{props.isAdmin && <i className="fa-solid fa-trash mx-2" onClick={()=>{handleDelete(props.media._id)}}></i>}</div></div></div>          
-          <div style={{cursor:"pointer"}} onClick={handleClick}>
-            <img src={props.media.thumbnail} alt="" width="340" height="240"/>
+    <div className="col-lg-3 col-md-4 col-sm-6">
+      <div className="card cd my-3">
+        <div className="card-body cd-bd">
+
+          <div style={{ cursor: "pointer" }} onClick={handleClick}>
+            <img className="img-pod" src={props.media.thumbnail} alt="" />
+          </div>
+
+          <span className="card-title cd-ttl">{props.media.name}</span>
+
+          <div className="flex-row gap-5">
+            <div>
+              <h5 className="card-text cd-spk">{props.media.speaker}</h5>
+              <div>
+                {props.isAdmin && <i className="fa-solid fa-trash mx-2" onClick={() => { handleDelete(props.media._id) }}></i>}
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/UploadsList.js
+++ src/components/UploadsList.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Cards } from './Cards'
+import "../static/index.css"
+import "../static/UploadList.css"
 
-const UploadList = ({medias, isAdmin, setMedias}) => {
+
+const UploadList = ({ medias, isAdmin, setMedias }) => {
   return (
-    <div className="container">
+    <>
+      <div className='podcats'>Best Episodes of the Week</div>
+      <div className="list-cont">
         <div className="row my-2">
-       {medias && medias.map((media)=>{
-        return <Cards isAdmin={isAdmin} key={media._id} media={media} setMedias={setMedias} medias={medias}/>
-       })}
-    </div>
-  </div>
+          {medias && medias.map((media) => {
+            return <Cards isAdmin={isAdmin} key={media._id} media={media} setMedias={setMedias} medias={medias} />
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
